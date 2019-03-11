21a9cf89965ae5d0a7b30917c3193bf8f78db77e
rrosztoczy <rrosztoczy@gmail.com>
2019-03-11 10:00:27 -0400

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do 
+        status 200
+        "Hello, world!"
+      end
+    
+      get '/name' do 
+        status 200
+        "My name is "
+      end
+    
+      get '/hometown' do 
+        status 200
+        "My hometown is "
+      end
+    
+      get '/favorite-song' do 
+        status 200
+        "My favorite song is "
+      end
+    
 end
